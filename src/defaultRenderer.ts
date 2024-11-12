f88f3e83bbba577e3770110db3234fc1c7626493
yuki <nahcikuy@gmail.com>
2024-11-12 16:52:31 +0800

    initial commit

---
+++ src/defaultRenderer.ts
@@ -0,0 +1,16 @@
+import { LogLevel, Renderer } from './index.js'
+import { IFont, font } from 'terminal-font'
+
+const styleMap: Record<LogLevel, IFont> = {
+  ALERT: font().yellow(),
+  CRITICAL: font().red(),
+  DEBUG: font().white(),
+  EMERGENCY: font().red(),
+  ERROR: font().red(),
+  INFO: font().green(),
+  NOTICE: font().green(),
+  WARN: font().yellow(),
+}
+
+export const defaultRenderer: Renderer = (level, content) =>
+  styleMap[level].apply(`[${new Date().toLocaleString('ja-jp')} ${level}] ${content}`)
---
+++ test/index.ts
@@ -0,0 +1,6 @@
+import { Elosonc } from '../src/index.js'
+
+const logger = Elosonc()
+logger.alert('222')
+logger.critical('3333')
+logger.debug('4444')
